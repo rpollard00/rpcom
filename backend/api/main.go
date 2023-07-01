b70c930ffc137f453861964512c04e94f562100a
Reese Pollard <rpollard@gmail.com>
2023-06-30 21:55:23 -0700

    Begin to create a real backend

---
+++ backend/api/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	addr := flag.String("addr", ":4000", "HTTP Network Address:[port]")
+	flag.Parse()
+
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/api/", home)
+	mux.HandleFunc("/api/blog", getBlog)
+	mux.HandleFunc("/api/blog/post", postBlog)
+
+	srv := &http.Server{
+		Addr:     *addr,
+		ErrorLog: errorLog,
+		Handler:  mux,
+	}
+
+	infoLog.Printf("Starting server on address: %s ", *addr)
+
+	err := srv.ListenAndServe()
+
+	errorLog.Fatal(err)
+}
