d009d4b168969700cac9c7ea32e73386a5931696
Reese Pollard <rpollard@gmail.com>
2023-07-02 22:23:26 -0700

    Begin to set up DB connectivity

--- backend/api/handlers.go
+++ backend/api/handlers.go
@@ -2,40 +2,39 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 	"strconv"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
-	log.Print("HTTP METHOD: ", r.Method)
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
+	app.infoLog.Print("HTTP METHOD: ", r.Method)
 
 	if r.URL.Path != "/api/" {
-		http.NotFound(w, r)
+		app.notFound(w)
 		return
 	}
 }
 
-func getBlog(w http.ResponseWriter, r *http.Request) {
+func (app *application) getBlog(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 
 	if err != nil || id < 1 {
-		http.NotFound(w, r)
+		app.notFound(w)
 		return
 	}
 
-	log.Print("HTTP METHOD: ", r.Method)
-	log.Print("id? =  ", id)
+	app.infoLog.Print("HTTP METHOD: ", r.Method)
+	app.infoLog.Print("id? =  ", id)
 	fmt.Fprintf(w, "Read a specific blog with id %d...\n", id)
 
 }
 
-func postBlog(w http.ResponseWriter, r *http.Request) {
-	log.Print("HTTP METHOD: ", r.Method)
+func (app *application) postBlog(w http.ResponseWriter, r *http.Request) {
+	app.infoLog.Print("HTTP METHOD: ", r.Method)
 
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", http.MethodPost)
-		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		app.clientError(w, http.StatusMethodNotAllowed)
 	}
 
 	w.Write([]byte("Create a new blog...\n"))
