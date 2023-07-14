91dbaff933b49751138c58bbf3e654d60fe972da
Reese Pollard <rpollard@gmail.com>
2023-07-13 22:22:12 -0700

    Adjust portrait idk

--- app/src/components/Home.tsx
+++ app/src/components/Home.tsx
@@ -6,16 +6,16 @@ export const Home = () => {
   return (
     <div className="flex flex-col md:flex-row items-start">
       <div className="p-2 md:mb-0 md:mr-6">
-        <div className="bg-cyan-400 w-[150px] h-[200px] border-solid rounded mb-2">
+        <div className="flex flex-col bg-cyan-400 w-[150px] h-[200px] border-solid rounded-full mb-2">
           <img
-            className="object-cover object-[50%_40%] w-[100%] h-[100%] rounded"
+            className="flex-none object-cover object-[50%_40%] w-[100%] h-[100%] rounded-full"
             src={portraitImg}
             alt="Cool picture"
           />
-        </div>
-        <a className="text-cyan-400" href="mailto://reese@reesep.com">
+        <a className="mt-3 text-cyan-400 block self-center" href="mailto://reese@reesep.com">
           Contact
         </a>
+        </div>
       </div>
       <div className="p-2 min-w-[70%]">
         <Title>Reese Pollard</Title>
