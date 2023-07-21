b6359921f248a27bc69eeddc14d8f0a04376c78f
Reese Pollard <rpollard@gmail.com>
2023-07-20 21:46:42 -0700

    Make less insane

--- app/src/components/Blog.tsx
+++ app/src/components/Blog.tsx
@@ -2,24 +2,23 @@ import { useEffect, useState } from 'react'
 import blogService from '../services/blogs'
 
 const Blog = () => {
-  const [blogEntry, setBlogEntry] = useState({
-    loading: false,
-    blog: { Title: "", Author: "", Content: ""},
-  })
+  const [loading, setLoading] = useState(false)
+  const [blogEntry, setBlogEntry] = useState({ Title: "", Author: "", Content: ""})
 
   useEffect(() => {
-    setBlogEntry({ loading: true, blog: blogEntry.blog })
+    setLoading(true)
     blogService.getBlogs().then(r => {
-      setBlogEntry({ loading: false, blog: r.data})
+      setBlogEntry({ ...r.data})
+      setLoading(false)
     })
   }, [setBlogEntry])
   return (
     <>
-      { blogEntry.loading ? <div>Loading...</div> :  
+      { loading ? <div>Loading...</div> :  
       <div className="flex flex-col items-center">
-        <h2 className="text-2xl font-bold p-3">{blogEntry.blog.Title} - {blogEntry.blog.Author}</h2>
+        <h2 className="text-2xl font-bold p-3">{blogEntry.Title} - {blogEntry.Author}</h2>
         <div>
-          {blogEntry.blog.Content}
+          {blogEntry.Content}
         </div>
       </div> }
     </>
